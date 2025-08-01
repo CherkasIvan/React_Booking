90c7197450b6efe3d0160a469b56193e9cbbdd38
JV13 <cherkas.ivan13@gmail.com>
2025-08-01 20:01:39 +0300

    build

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,31 +1,27 @@
-import React, {Component} from 'react';
-import {Card, Container} from 'semantic-ui-react';
-import axios from 'axios';
-
-import {BookCardContainer, FilterContainer, MenuBaseContainer} from '../../containers';
-
+import React, { Component } from 'react';
+import { Card, Container } from 'semantic-ui-react';
+import booksData from './public/books.json';
+import { BookCardContainer, FilterContainer, MenuBaseContainer } from '../../containers';
 
 class App extends Component {
-  componentWillMount() {
-    const {setBooks} = this.props;
-    axios.get('books.json').then(({data}) => {
-      setBooks(data);
-    });
+  componentDidMount() { 
+    const { setBooks } = this.props;
+    setBooks(booksData); 
   }
 
   render() {
-    const {books, isReady} = this.props;
+    const { books, isReady } = this.props;
 
     return (
-        <Container>
-          <MenuBaseContainer/>
-          <FilterContainer/>
-          <Card.Group itemsPerRow={4}>
-            {!isReady
-              ? 'Загрузка...'
-              : books.map((book, i) => <BookCardContainer key={i} {...book} />)}
-          </Card.Group>
-        </Container>
+      <Container>
+        <MenuBaseContainer />
+        <FilterContainer />
+        <Card.Group itemsPerRow={4}>
+          {!isReady
+            ? 'Загрузка...'
+            : books.map((book, i) => <BookCardContainer key={i} {...book} />)}
+        </Card.Group>
+      </Container>
     );
   }
 }
